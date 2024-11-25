41fd8cdf74dcfec3327a052fa5f1bd126c9808fd
rueAlter67 <94287905+rueAlter67@users.noreply.github.com>
2024-11-25 19:32:58 +0800

    code for input matrix in c

---
+++ matrixAccel.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+extern double getVelocity();
+extern int getAcceleration();
+
+
+int main(){
+	int totalCars;
+	int col = 3; 
+	
+	int j,k;
+	
+	scanf("%d", &totalCars);
+	double carMatrix[totalCars][col];
+	
+	for(j=0; j<totalCars; j++){
+		for(k=0; k<col; k++){
+			scanf("%lf", & carMatrix[j][k]);
+		}
+	}
+}
