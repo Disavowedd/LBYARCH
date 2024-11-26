2f6d5cda11a7b4972a5b8630925d793d620f5e25
Disavowedd <disavowed19@gmail.com>
2024-11-26 23:31:01 +0800

    added km/h and memory assign
    tested with following inputs:
    1
    62.5 0.0 10.1
    OUTPUT = 6.19

--- matrixaccel.asm
+++ matrixaccel.asm
@@ -7,3 +7,7 @@ global getAcceleration
 
 
 getVelocity:
+    ;xmm0 = iniVelocity, xmm1 = finVelocity, xmm2 = time
+    subss xmm0, xmm1
+    divss xmm0, xmm2 ;this is Km/h
+    ret
--- matrixaccel_c.c
+++ matrixaccel_c.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-extern double getVelocity();
+extern float getVelocity(float initialVelocity, float finalVelocity, float time);
 extern int getAcceleration();
 
 
@@ -20,4 +20,18 @@ int main(){
 			scanf("%lf", &carMatrix[j][k]);
 		}
 	}
+	 float initialVelocity, finalVelocity, time, velocity;
+	 for (j = 0; j < totalCars; j++) {
+        initialVelocity = carMatrix[j][0];
+        finalVelocity = carMatrix[j][1];
+        time = carMatrix[j][2];
+		printf("Car %d -> Initial Velocity: %.2f, Final Velocity: %.2f, Time: %.2f\n", j + 1, initialVelocity, finalVelocity, time); //for debugging only
+	 	
+	    velocity = getVelocity(initialVelocity, finalVelocity, time);
+        printf("Car %d Velocity: %.2f\n", j + 1, velocity); 
+	 }
+
+
+	return 0;
+
 }
