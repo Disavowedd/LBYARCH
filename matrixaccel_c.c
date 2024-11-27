83467e38d1a0f56a27c744ed53c3b15a44fe9b98
Disavowedd <disavowed19@gmail.com>
2024-11-27 21:47:07 +0800

    for avg time taken

--- matrixaccel_c.c
+++ matrixaccel_c.c
@@ -6,41 +6,46 @@
 
 extern void getAcceleration(int n, double* matrix, int* ans);
 
+int main() {
+    int totalCars, num, nElements;
+    int col = 3;
 
-int main(){
-	int totalCars, num, nElements;
-	int col = 3; 
-	
-	int j,k;
-	
-	scanf("%d", &totalCars);
-	nElements = totalCars * col;
-	num = totalCars;
-	
-	int* ans =(int*)malloc(num*sizeof(int));
-	double* carMatrix =(double*)malloc(nElements*sizeof(double));
+    int j, k;
 
-	//get input
-	for(k=0; k<nElements; k++){
-		scanf("%lf", &carMatrix[k]); 
-	}
+    
+    scanf("%d", &totalCars);
+    nElements = totalCars * col;
+    num = totalCars;
 
-		struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-	//convert to proper values
-	getAcceleration(num,carMatrix,ans);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	long seconds = end.tv_sec - start.tv_sec;
-    long nanoseconds = end.tv_nsec - start.tv_nsec;
+    int* ans = (int*)malloc(num * sizeof(int));
+    double* carMatrix = (double*)malloc(nElements * sizeof(double));
 
-	for(k=0; k<num; k++){
-		printf("%d - acceleration = %d\n", k, ans[k]);
-	}
+   
+    for (k = 0; k < nElements; k++) {
+        scanf("%lf", &carMatrix[k]);
+    }
 
-	printf("Time taken: %ld seconds %ld nanoseconds\n", seconds, nanoseconds);
-	 
-	free(carMatrix);
-	
+    long totalNanoseconds = 0;
 
-	return 0;
+
+    for (j = 0; j < 30; j++) {
+        struct timespec start, end;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        // Convert to proper values
+        getAcceleration(num, carMatrix, ans);
+
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        long seconds = end.tv_sec - start.tv_sec;
+        long nanoseconds = end.tv_nsec - start.tv_nsec;
+        totalNanoseconds += seconds * 1000000000L + nanoseconds;
+    }
+
+    // Output average time taken
+    printf("Average time taken: %ld nanoseconds\n", totalNanoseconds / 30);
+
+    free(ans);
+    free(carMatrix);
+
+    return 0;
 }
